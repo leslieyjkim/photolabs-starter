989697f56d0bcaefdc113134820ad2ff403bc533
yoonjookim <yjblossom@gmail.com>
2024-01-10 22:40:58 -0500

    Revise PhotoFavButton & PhotoListItem

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,17 +1,13 @@
-import React, { useCallback, useState } from 'react';
-
+import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 const PhotoFavButton = ({ isFavourite, updateToFavPhotoIds }) => {
   return (
     <div className='photo-list__fav-icon' onClick={updateToFavPhotoIds}>
-      <div className='photo-list__fav-icon-svg'>
-        <FavIcon selected={isFavourite} />
-      </div>
+      <FavIcon selected={isFavourite} />
     </div>
   );
 };
-
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 //it appears that the class names defined in your Sass file are not matching the class names used in your JSX.
 //To apply the Sass styles correctly, you need to ensure that the class names in your JSX match the BEM (Block, Element, Modifier) structured class names in your Sass file.
 // Here's how you can update the PhotoListItem.jsx to use the class names defined in the PhotoListItem.scss file:
 
 const PhotoListItem = ({ photoData }) => {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  // Handler to toggle the favorite status
+  const toggleFavoriteStatus = useCallback(() => {
+    setIsFavorited(currentState => !currentState);
+    // Here you can also perform additional tasks, such as updating the parent state or making an API call
+  }, []);
+
   return (
     <div className="photo-list__item">
-      <img className="photo-list__image" src={photoData.imageSource} alt="Sample Photo" />
+      <div className="photo-list__image-container">
+        <img className="photo-list__image" src={photoData.imageSource} alt="Sample Photo" />
+        <PhotoFavButton isFavourite={isFavorited} updateToFavPhotoIds={toggleFavoriteStatus} />
+      </div>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photoData.profile} alt={photoData.username} />
         <div className="photo-list__user-info">
