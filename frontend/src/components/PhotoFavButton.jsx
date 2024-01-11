454c2c9b1e9e650089ed4154fe7dcb11e4b74c5a
yoonjookim <yjblossom@gmail.com>
2024-01-10 21:18:06 -0500

    Add fav button on Photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,15 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = ({ isFavourite, updateToFavPhotoIds }) => {
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    <div className='photo-list__fav-icon' onClick={updateToFavPhotoIds}>
+      <div className='photo-list__fav-icon-svg'>
+        <FavIcon selected={isFavourite} />
       </div>
     </div>
   );
-}
+};
+
 
 export default PhotoFavButton;
