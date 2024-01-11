345fe4c0f3f9a423e878b0c6f11b70726a7149b1
yoonjookim <yjblossom@gmail.com>
2024-01-11 13:51:03 -0500

    Add PhotoList Componenet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
+// import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoList from '.componenets/PhotoList';
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 
 
 const App = () => {
   // Create an array with 3 elements, all of them are sampleDataForPhotoListItem
   // const photos = new Array(3).fill(sampleDataForPhotoListItem);
-  const photos = [...Array(3)].map(() => sampleDataForPhotoListItem);
+  // const photos = [...Array(3)].map(() => sampleDataForPhotoListItem);
 
   return (
     <div className="App">
-      {photos.map((photoData, index) => (
+      {/* {photos.map((photoData, index) => (
         // Pass each item of the array to PhotoListItem as a prop and rename the prop to photoData
         <PhotoListItem key={photoData.id + index} photoData={photoData} />
-      ))}
+      ))} */}
+
+      <PhotoList />
     </div>
   );
 };
