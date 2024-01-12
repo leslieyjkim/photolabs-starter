7c1ab3e9a1b8b8c9cd4ec6853db2693d20c5156d
yoonjookim <yjblossom@gmail.com>
2024-01-12 18:20:43 -0500

    Update App.jsx to manage a state for displaying modal(hide/show)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
 import topics from './mocks/topics';
 import photos from './mocks/photos';
 
@@ -8,10 +9,12 @@ import photos from './mocks/photos';
 
 
 const App = () => {
-
+  const [displayModal, setDisplayModal] = useState(false);
+  
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} />
+      <HomeRoute topics={topics} photos={photos} setDisplayModal={setDisplayModal} />
+      {displayModal && <PhotoDetailsModal closeModal={() => setDisplayModal(false)} />}
     </div>
   );
 };
